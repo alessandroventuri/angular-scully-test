57413ea45546cdeca5ca2a2e4249e5f59aa1ebb0
alexvent10 <xalessandroventuri@gmail.com>
2022-04-14 11:40:28 +0200

    added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '[app-root]',
-  template: `<router-outlet></router-outlet>`,
+  template: `
+    <header>
+      <h1>Angular-Scully test!</h1>
+    </header>
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {}
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   template: `
-    <h1>Homepage</h1>
+    <h2>Homepage</h2>
     <nav>
       <ul>
         <li><a href="/first">First Page</a></li>
